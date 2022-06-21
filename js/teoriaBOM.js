bd1e55c4d76aa139904d244501c66bbeaf8e9a0b
Juan Menichetti <jpmenichetti13@gmail.com>
2022-06-21 10:52:45 -0300

    se agrego la teoria de setTimeout y setInterval

---
+++ js/teoriaBOM.js
@@ -0,0 +1,29 @@
+let contador = 1;
+
+function mostrarPublicidad(){
+    if(contador === 5){
+        // quiero detener el intervalo
+        window.clearInterval(codigo);
+    }
+    document.write('<p>Ahora muestro publicidad</p>');
+    contador++
+}
+
+// ejecutar una funcion pasado cierto tiempo en milisegundos
+
+// forma 1:
+// window.setTimeout(mostrarPublicidad, 3000);
+
+// forma 2:
+// window.setTimeout(function(){
+//     document.write('<p>Ahora muestro publicidad</p>');
+// },4000)
+
+// forma 3:
+// setTimeout(()=>{document.write('<p>Ahora muestro publicidad</p>');},3500);
+
+// ejecutar una funcion cada cierto tiempo en milisegundos
+let codigo = setInterval(mostrarPublicidad,3000);
+
+
+
